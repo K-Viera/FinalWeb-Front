49cad01c5351511545ca982c653b909c55031cb9
Kevin Viera <53414453+K-Viera@users.noreply.github.com>
2022-05-18 13:25:34 -0500

    change method

--- src/global.js
+++ src/global.js
@@ -1,6 +1,6 @@
 let global={};
 
 // global.url="https://mision-request.herokuapp.com";
- global.url="http://localhost:5000"
-
+//  global.url="http://localhost:5000"
+global.url = "https://finalweb-back.herokuapp.com";
 module.exports=global;
